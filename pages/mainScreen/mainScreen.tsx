d8f9b0faa3bc858811dfe99f68d78bfa9c64d4eb
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-24 15:00:29 +0200

    REFORMAT, main screen component

--- pages/mainScreen/mainScreen.tsx
+++ pages/mainScreen/mainScreen.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import styles from '../styles/MainScreen.module.css'
+import React, { useEffect, useState } from 'react';
+import FormComponent from '../../components/FormComponent/FormComponent';
+import QueryInterface from '../../interfaces/QueryInterface';
+import RecipeInterface from '../../interfaces/RecipeInterface';
+import Emitter from '../../services/EventEmitterSrv';
+import FoodSrv from '../../services/FoodSrv';
 
-const mainScreen = () => {
+
+export default function MainScreen() {
+  const [query, setQuery] = useState<string>('');
+  const [recipe, setRecipe] = useState<RecipeInterface | {}>({});
+
+  Emitter.on('FORM-SUBMITTED', ({ query }) => {
+    console.log(`query recieved`, query);
+    setQuery(query);
+  });
+
+  useEffect(() => {
+    const getRecipe = async (query: string) => {
+      const res = await FoodSrv.getRecipe(query);
+      console.log(`res`, res);
+      setRecipe(res);
+      console.log(`recipe`, recipe);
+    }
+
+    query && getRecipe(query);
+  }, [query])
+
   return (
     <div>
-      hello
+      <FormComponent></FormComponent>
     </div>
   );
 }
-
-export default mainScreen;
