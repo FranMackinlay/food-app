bf89782ba11fa7787aaf88a6c96cd58b4ae11870
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-24 15:59:08 +0200

    IMPROVE, main screen component

--- pages/mainScreen/mainScreen.tsx
+++ pages/mainScreen/mainScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import FormComponent from '../../components/FormComponent/FormComponent';
+import RecipesComponent from '../../components/RecipesComponent/RecipesComponent';
 import QueryInterface from '../../interfaces/QueryInterface';
 import RecipeInterface from '../../interfaces/RecipeInterface';
 import Emitter from '../../services/EventEmitterSrv';
@@ -8,19 +9,16 @@ import FoodSrv from '../../services/FoodSrv';
 
 export default function MainScreen() {
   const [query, setQuery] = useState<string>('');
-  const [recipe, setRecipe] = useState<RecipeInterface | {}>({});
+  const [recipes, setRecipes] = useState<RecipeInterface[]>([]);
 
   Emitter.on('FORM-SUBMITTED', ({ query }) => {
-    console.log(`query recieved`, query);
     setQuery(query);
   });
 
   useEffect(() => {
     const getRecipe = async (query: string) => {
       const res = await FoodSrv.getRecipe(query);
-      console.log(`res`, res);
-      setRecipe(res);
-      console.log(`recipe`, recipe);
+      setRecipes(res);
     }
 
     query && getRecipe(query);
@@ -29,6 +27,7 @@ export default function MainScreen() {
   return (
     <div>
       <FormComponent></FormComponent>
+      <RecipesComponent recipes={recipes}></RecipesComponent>
     </div>
   );
 }
