9b1875c0c9c1e317bc9f58db8b1cbdf5d6070179
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-24 12:47:30 +0200

    CREATE, main screen component and navigation

---
+++ pages/mainScreen/mainScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/MainScreen.module.css'
+
+const mainScreen = () => {
+  return (
+    <div>
+      hello
+    </div>
+  );
+}
+
+export default mainScreen;
