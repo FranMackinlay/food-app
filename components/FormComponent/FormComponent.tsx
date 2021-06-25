1772cf5fe231eebf22b9c278e82cc8c86cac4d8d
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-25 10:51:57 +0200

    IMPROVE, form component storage

--- components/FormComponent/FormComponent.tsx
+++ components/FormComponent/FormComponent.tsx
@@ -18,22 +18,8 @@ export default function FormComponent() {
 
   const [recipesHistory, setRecipesHistory] = useState<Value[]>([]);
 
-
-
   const updateQueryHistory = (value: Value) => {
-    const localHistory = localStorage.getItem('queryHistory');
-
-    if (localHistory) {
-      const searchHistory = JSON.parse(localHistory);
-
-      const queryHistory = Array.isArray(searchHistory) ? searchHistory : [searchHistory];
-      queryHistory.push(value);
-
-      localStorage.setItem('queryHistory', JSON.stringify(queryHistory));
-      setRecipesHistory(queryHistory.reverse());
-    } else {
-      localStorage.setItem('queryHistory', JSON.stringify([value]));
-    }
+    checkForLocalHistory(true, value);
   }
 
   const onFormikSubmit = async (value: Value, { setSubmitting, resetForm }: FormikHelpers<Value>) => {
@@ -53,19 +39,28 @@ export default function FormComponent() {
     setRecipesHistory([]);
   }
 
-  useEffect(() => {
+  const checkForLocalHistory = (update: boolean, value?: Value): void => {
     const localHistory = localStorage.getItem('queryHistory');
 
     if (localHistory) {
-
       const parsedHistory = JSON.parse(localHistory);
 
       const queryHistory = Array.isArray(parsedHistory) ? parsedHistory : [parsedHistory];
 
-      if (queryHistory.length) {
-        setRecipesHistory(queryHistory.reverse());
+      if (update) {
+        queryHistory.push(value);
+        localStorage.setItem('queryHistory', JSON.stringify(queryHistory));
       }
+
+      setRecipesHistory(queryHistory.reverse());
+
+    } else {
+      value && localStorage.setItem('queryHistory', JSON.stringify([value]));
     }
+  }
+
+  useEffect(() => {
+    checkForLocalHistory(false);
   }, []);
 
   return (
@@ -89,7 +84,7 @@ export default function FormComponent() {
         <Box ml={15} d="flex" alignItems="center" justifyContent={recipesHistory?.length ? 'space-between' : 'flex-end'}>
           <Box>
             {recipesHistory.slice(0, 10).map((oldQuery, index) => (
-              <Badge mr={3} className={styles.badge} onClick={() => onClickSelectOldQuery(oldQuery)} key={index} colorScheme="green">{oldQuery.query}</Badge>
+              <Badge mr={3} className={styles.badge} onClick={() => onClickSelectOldQuery(oldQuery)} key={index} colorScheme="green">{oldQuery?.query}</Badge>
             ))}
           </Box>
           <Button onClick={clearHistory} colorScheme="teal" size="xs" variant="outline">
